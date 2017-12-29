5d9050144cedd4e80e6dcc8738c9dfadc0068ae8
Niopullus <owenvnek@gmail.com>
2017-12-28 23:08:56 -0500

    Created basic server structure.

---
+++ Control Panel/src/ConnectionManager.java
@@ -0,0 +1,12 @@
+import java.io.OutputStream;
+import java.util.List;
+
+public class ConnectionManager {
+
+    private List<OutputStream> clientOutputStreams;
+
+    public ConnectionManager(final PanelConfig config) {
+
+    }
+
+}
---
+++ Control Panel/src/PanelConfig.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class PanelConfig {
+
+    public PanelConfig(final File source) {
+
+    }
+
+    public int getPort() {
+        return 0; //temp
+    }
+
+}
